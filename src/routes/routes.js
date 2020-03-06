12db5d61279382ceff45f0b125672c6b1217dd38
OSCAR ARMANDO REINOSA BOLANOS <oscar.reinosa@telusinternational.com>
2020-03-06 17:06:37 -0600

    -including auth routes in /auth

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const usersRouter = require('./users.router');
-const notesRouter = require('./notes.router');
+const authRouter = require('./auth.router');
 // add user routes to /users
 router.use('/users', usersRouter);
-// router.use('/notes', notesRouter);
+router.use('/auth', authRouter);
 
 // export all routes
 module.exports = router;
