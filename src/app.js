810bd53ade1a557bafc31add2d8fea75750f1ff6
OSCAR ARMANDO REINOSA BOLANOS <oscar.reinosa@telusinternational.com>
2020-03-06 17:04:10 -0600

    added dotenv

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
+require('dotenv').config();
+
 // init mongoose Singleton
 require('./config/mongoose');
 // main router to import all routes
