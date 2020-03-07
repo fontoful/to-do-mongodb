7e0968749a06091525285a61de172deffb5fb418
oreinosa <31995668+oreinosa@users.noreply.github.com>
2020-03-06 22:27:49 -0600

    -added config file

---
+++ src/config/config.js
@@ -0,0 +1,19 @@
+module.exports = function () {
+  switch (process.env.NODE_ENV) {
+    case 'development':
+      return {
+        DB_HOST: process.env.DEV_DB_HOST,
+        DB_PORT: process.env.DEV_DB_PORT,
+        DB_NAME: process.env.DB_NAME,
+        SECRET: process.env.SECRET
+      };
+
+    case 'production':
+      return {
+        DB_USER: process.env.PROD_DB_USER,
+        DB_PASS: process.env.PROD_DB_PASS,
+        DB_NAME: process.env.DB_NAME,
+        SECRET: process.env.SECRET,
+      };
+  }
+};
