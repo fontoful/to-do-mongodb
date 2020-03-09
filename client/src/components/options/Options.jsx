221c067a27bc9ecbaffcbc85204a96c07c4eca8f
Hector Alfonso Serrano Castro <hector.serrano@telusinternational.com>
2020-03-09 11:30:47 -0600

    fixed styles of left bar

--- client/src/components/options/Options.jsx
+++ client/src/components/options/Options.jsx
@@ -2,27 +2,21 @@ import React from "react";
 import "./Options.css";
 
 export const Options = () => {
-	return (
-		<form>
-			<div className="options">
-				<div className="add-todo">
-					<input
-						type="text"
-						name="input"
-						placeholder="Add a new to-do"
-					/>
-				</div>
-				<div className="lists">
-					<h2>LISTS</h2>
-					<nav>
-						<ul>
-							<li>Home</li>
-							<li>Work</li>
-							<li>Studies</li>
-						</ul>
-					</nav>
-				</div>
-			</div>
-		</form>
-	);
+    return (
+        <div className="options">
+            <form>
+                <input type="text" name="input" placeholder="Add a new to-do" />
+            </form>
+            <div className="lists">
+                <h2>LISTS</h2>
+                <nav>
+                    <ul>
+                        <li>Home</li>
+                        <li>Work</li>
+                        <li>Studies</li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    );
 };
