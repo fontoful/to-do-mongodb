a0cbe6ddd032fbb13b46f891df5b79c1f9f608bf
OSCAR ARMANDO REINOSA BOLANOS <oscar.reinosa@telusinternational.com>
2020-03-09 11:08:57 -0600

    -added documentation and corrected error mesages

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,7 +1,15 @@
+/**
+ * Function to ensure request is authenticated
+ * @param {Request} req 
+ * @param {Response} res 
+ * @param {Function} next 
+ */
 const ensureAuthenticated = (req, res, next) => {
+  // if account is authenticated
   if (req.isAuthenticated()) {
     return next();
   } else {
+    // if not authenticated, return error message and 401 code
     req.flash('error_msg', 'Please log in to view that resource');
     res.status(401).json({
       message: "Unauthorized",
@@ -9,12 +17,19 @@ const ensureAuthenticated = (req, res, next) => {
     })
   }
 };
+/**
+ * Function to ensure request is not authenticated
+ * @param {Request} req 
+ * @param {Response} res 
+ * @param {Function} next 
+ */
 const forwardAuthenticated = (req, res, next) => {
+  // if not authenticated, allow next function
   if (!req.isAuthenticated()) {
     return next();
   }
 };
-
+// export functions
 module.exports = {
   ensureAuthenticated,
   forwardAuthenticated
