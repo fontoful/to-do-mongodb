88f117a66cfa463995e0bef3bf2a01c954478167
OSCAR ARMANDO REINOSA BOLANOS <oscar.reinosa@telusinternational.com>
2020-03-09 09:35:53 -0600

    removed users routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,10 @@
 const router = require('express').Router();
-const usersRouter = require('./users.router');
+// const usersRouter = require('./users.router');
 const authRouter = require('./auth.router');
 const notesRouter = require('./notes.router');
-const { forwardAuthenticated, ensureAuthenticated } = require('../config/auth');
 
 // add user routes to /users
-router.use('/users', usersRouter);
+// router.use('/users', usersRouter);
 router.use('/auth', authRouter);
 router.use('/notes', notesRouter);
 
