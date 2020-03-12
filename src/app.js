aa02553afb320697f1c8906a39be51fbcb9661e9
OSCAR ARMANDO REINOSA BOLANOS <oscar.reinosa@telusinternational.com>
2020-03-12 15:35:27 -0600

    added cors, sanitize, ddos and removed express-session and connect-flash

--- src/app.js
+++ src/app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const passport = require('passport');
-const session = require('express-session');
-const flash = require('connect-flash');
+const cors = require('cors');
+const sanitize = require('sanitize');
 const swagger = require('swagger-ui-express');
 const swaggerConfig = require('./swagger.json');
+const Ddos = require('ddos');
+const ddos = new Ddos({ burst: 10, limit: 15 })
+
 require('dotenv').config();
 
 // init mongoose Singleton
@@ -17,27 +20,25 @@ require('./config/passport')(passport);
 const mainRouter = require('./routes/routes');
 // init express app
 const app = express();
+// add sanitize
+app.use(sanitize.middleware);
 // add logging
 app.use(logger('dev'));
 // add json support
 app.use(express.json());
-
-app.use(
-  session({
-    secret: process.env.SECRET,
-    resave: true,
-    saveUninitialized: true
-  })
-);
-
+// add cors
+app.use(cors({
+  origin: ["http://localhost:4200"],
+  credentials: true
+}))
+// add ddos 
+app.use(ddos.express);
 //swagger config
 app.use('/swagger', swagger.serve, swagger.setup(swaggerConfig));
 
-app.use(flash());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
-app.use(passport.session());
 // using router in /api/v1 route
 app.use('/api/v1', mainRouter);
 
